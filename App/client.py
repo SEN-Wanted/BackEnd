8718f06b0ba4bc6058a45dfb8e365e54a4cd2389
t617 <t617@qq.com>
2018-05-12 18:03:42 +0800

    完成run.py以及添加client.py测试

---
+++ App/client.py
@@ -0,0 +1,11 @@
+import requests
+
+user_info = {'phone_num': '110', 'password': '123'}
+# cookies = {'phone_num': 'letian', 'password': '123'}
+r = requests.post("http://127.0.0.1:5000/register", data=user_info)
+# l = requests.post("http://127.0.0.1:5000/login", cookies=cookies)
+
+
+# print (r.text)
+print (r.cookies['phone_num'], r.cookies['password'])
+# print (l.text)
