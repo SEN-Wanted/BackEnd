f6f3337c52c11aaabea54d15d300d65d0f81bec3
t617 <t617@qq.com>
2018-06-19 19:20:56 +0800

    token认证

--- App/client.py
+++ App/client.py
@@ -1,10 +1,24 @@
 import requests
 
-user_info = {'phone': '110', 'password': '123'}
-# cookies = {'phone_num': 'letian', 'password': '123'}
-r = requests.post("http://127.0.0.1:5000/sign_up", data=user_info)
-# l = requests.post("http://127.0.0.1:5000/login", cookies=cookies)
+data = "{'username': '111', 'password': '222'}"
+user_info = {'username': '111', 'password': '222'}
+# user_info = {'phone': '110', 'password': '123'}
+# params = {'type': 'dessert'}
+token ="eyJhbGciOiJIUzI1NiIsImV4cCI6MTUyOTQwNTc0MSwiaWF0IjoxNTI5NDA1MTQxfQ.eyJpZCI6MX0.WzZyCbI_g7rtpCRWmB360eYTxbk-Rk7kQswYhZsjm3k"
+headers = {'accesstoken':token}
+# r = requests.post("http://127.0.0.1:5000/login", data=user_info, headers=headers)
+r = requests.post("http://127.0.0.1:5000/user/110/orders", data=data, headers=headers)
+# r = requests.get("http://127.0.0.1:5000/user/110/orders/1", headers=headers)
+# r = requests.post("http://127.0.0.1:5000/sign_up", data=user_info)
+# r = requests.get("http://127.0.0.1:5000/api/token",auth=(user_info['username'],user_info['password']))
+# r = requests.get("http://127.0.0.1:5000/api/resource",auth=(token, '222'))
+
+# s = requests.get("http://127.0.0.1:5000/search", params=params)
 
 print (r.text)
 # print (r.cookies['phone_num'], r.cookies['password'])
 # print (l.text)
+
+# real_ip = request.headers.get('X-Real-Ip', request.remote_addr)
+ # ip = request.remote_addr
+ # request.headers['Authorization']
